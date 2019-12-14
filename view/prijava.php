07fbafcb3f83031554016ce80e6159f6635a02a2
ma6068 <ma6068@student.uni-lj.si>
2019-12-14 22:44:20 +0100

    registracija i prijava izgled

--- view/prijava.php
+++ view/prijava.php
@@ -1,9 +1,54 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<title>Login</title>
+        <link rel="stylesheet" type="text/css" href="./css/all.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 
-echo ("ja sum prijava");
+
+<body>
+	<div class="spacing-bottom">
+		<div class="container">
+			<div class=" row flex-center">
+				<div class="col-md-4 oblik-prijava">
+					<div class=" mt-4">
+						<div class="color-bukvi">
+							<h2 class="card-title mt-2 text-center">Login</h2>
+						</div>
+						<article class="card-body">
+							<form role="form">
+								<div class="form-group">
+									<input id="email" placeholder="E-mail" class="form-control" type="email" maxlength="30">
+								</div>
+								<div class="form-group">
+									<input id="pass" placeholder="Password" class="form-control" type="password" maxlength="30">
+									<a href="">Forgot password?</a>
+								</div>
+								<div class="form-group" id="loginButton">
+									<button class="btn btn-primary btn-block" type="submit">Login</button>
+								</div>
+								<div class="form-group">
+									<p>Don't have an account? <a href="">Create one!</a></p>
+								</div>
+							</form>
+						</article>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+
+
+</body>
+
+
+
+</html>
--- view/registracija.php
+++ view/registracija.php
@@ -1,9 +1,66 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="./css/all.css">
+    <meta charset="utf-8">
+    <title>Registration</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 
-echo("ja sum registracija");
+
+<body>
+    <div>
+        <div class="container spacing-bottom">
+            <div class=" row flex-center">
+                <div class="col-md-4 oblik-prijava">
+                    <div class=" mt-4">
+                        <div class="color-bukvi">
+                            <h2 class="card-title mt-2 text-center">Registration</h2>
+                        </div>
+                        <article class="card-body">
+                            <form role="form">
+                                <div class="form-group">
+                                    <input placeholder="Name" class="form-control" type="text" maxlength="20">
+                                </div>
+                                <div class="form-group">
+                                    <input placeholder="Surname" class="form-control" type="text" maxlength="20">
+                                </div>
+                                <div class="form-group" id="PhoneNumber">
+                                    <input placeholder="Phone number" class="form-control" type="text" maxlength="30">
+                                </div>
+                                <div class="form-group" id="Email">
+                                    <input placeholder="E-mail" class="form-control" type="email" maxlength="30">
+                                </div>
+                                <div class="form-group" id="Password">
+                                    <input placeholder="Password" class="form-control" type="password" maxlength="30">
+                                </div>
+                                <div class="form-group" id="Repeat Password">
+                                    <input placeholder="Repeat password" class="form-control" type="password" maxlength="30">
+                                </div>
+                                <div class="form-group" id="dugme">
+                                    <button class="btn btn-primary btn-block" type="submit"> Register</button>
+                                </div>
+                                <div class="form-group">
+                                    <p>Already have an account? <a href="/netbeans/EP/view/prijava.php">Log in</a></p>
+                                </div>
+                            </form>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+
+</body>
+
+
+
+</html>
