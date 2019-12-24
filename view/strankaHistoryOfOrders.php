a62012e56e8724811ea764c6881c6baf50609b08
ma6068 <ma6068@student.uni-lj.si>
2019-12-24 19:17:57 +0100

    popravki history za stranka

--- view/strankaHistoryOfOrders.php
+++ view/strankaHistoryOfOrders.php
@@ -5,20 +5,19 @@
     $idUporabnik = $_SESSION['id_uporabnik'];
     $sum = 0;
     
-    $query = "SELECT a.marka, a.cena, a.slika, a.opis, k.datum, k.kolicina "
-            . "FROM avto a, kosarica k, kosarica_avto ka, uporabnik u "
-            . "WHERE u.id_uporabnik=$idUporabnik AND k.status!='oddano' AND k.id_kosarica=ka.fk_id_k AND ka.fk_id_a=a.id_avto AND u.id_uporabnik=k.fk_id_uporabnik";
+    $query = "SELECT a.marka, a.cena, a.slika, a.opis, k.datum, k.kolicina, k.id_kosarica FROM avto a, kosarica k, kosarica_avto ka, uporabnik u WHERE u.id_uporabnik='$idUporabnik' AND k.status!='oddano' AND k.id_kosarica=ka.fk_id_k AND ka.fk_id_a=a.id_avto AND u.id_uporabnik=k.fk_id_uporabnik";
+
     $rezultat = mysqli_query($conn, $query);
     $podatoci = mysqli_num_rows($rezultat);
     
     if ($podatoci > 0) {
         $brojPoracka = 0;
-        $datumSega = '';
+        $idSega = '';
         $prvPodatok = 'da';
         while ($podatok = mysqli_fetch_assoc($rezultat)) {
             $imaPromena = 'ne';
-            if ($datumSega != $podatok['datum']) {
-                $datumSega = $podatok['datum'];
+            if ($idSega != $podatok['id_kosarica']) {
+                $idSega = $podatok['id_kosarica'];
                 $brojPoracka = $brojPoracka + 1;
                 $imaPromena = 'da';
                 if ($prvPodatok == 'da') {
