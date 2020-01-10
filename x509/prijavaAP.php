2b97c0c4928194d0411246988147fabb359f744c
ma6068 <ma6068@student.uni-lj.si>
2020-01-10 15:36:48 +0100

    fixes

--- x509/prijavaAP.php
+++ x509/prijavaAP.php
@@ -4,7 +4,7 @@
 	header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
         exit();
     }	
-    include './glava.php';
+    include 'glava.php';
 ?>
 
 <!DOCTYPE html>
--- x509/sertifikat.php
+++ x509/sertifikat.php
@@ -1,6 +1,6 @@
 <?php 
     session_start();
-    include 'konekcija.php';
+    include '../view/konekcija.php';
 ?>
 <html>
     <head>
